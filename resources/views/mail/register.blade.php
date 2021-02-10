0d84c46d1e3f59d8b87b7926ad047be2d134eb35
arseniy <arseniy@code.ripper>
2021-02-11 03:14:43 +0500

    Отправка письма при регистрации

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <h2>
+        Привет {{$data['name']}}, мы рады, что ты с нами! Ниже приведены данные вашего аккаунта:
+    </h2>
+    <br>
+    <h3>Email: </h3>
+    <p>{{$data['email']}}</p>
+    <h3>Username: </h3>
+    <p>{{$data['name']}}</p>
+    </body>
+    </html>
+</DOCTYPE>
